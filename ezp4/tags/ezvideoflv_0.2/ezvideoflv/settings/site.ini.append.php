9160ff6deb7c70c5138e779b0b13164465ce343a
Damien Pobel <damien@pobel.fr>
2009-10-04 13:15:58 +0000

    Added tag for 0.2 version
    git-svn-id: http://svn.projects.ez.no/ezvideoflv@19 881ed479-1dcf-4951-a8bd-c99831cb55be

---
+++ ezp4/tags/ezvideoflv_0.2/ezvideoflv/settings/site.ini.append.php
@@ -0,0 +1,15 @@
+<?php /*
+
+[RegionalSettings]
+TranslationExtensions[]=ezvideoflv
+
+#######################################################################
+# if you want to give read access to videos to all users (anonymous and
+# identified), you can uncomment the following two lines so that there's
+# no need to add video/read function in users' roles
+# (point 7 in http://projects.ez.no/ezvideoflv#eztoc7442_3)
+#######################################################################
+#[RoleSettings]
+#PolicyOmitList[]=video
+ 
+*/ ?>
